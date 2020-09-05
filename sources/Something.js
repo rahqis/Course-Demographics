77f153392778158eef93118cdb5b3652bae0bb89
rahqis <rahqi@ad.unc.edu>
2020-09-04 23:58:11 -0400

    master

--- sources/Something.js
+++ sources/Something.js
@@ -1,5 +1,5 @@
 document
-this.#document
+this.document
 
 var fName = document.getElementById("fName");
 var lName = document.getElementById("lName");
@@ -8,31 +8,37 @@ var inputGroupSelect = document.getElementById("inputGroupSelect");
 var password = document.getElementById("password");
 var cPassword = document.getElementById("cPassword");
 var signup = document.getElementById("signup");
+var email;
 
-var profile = {
-    fName,
-    lName,
-    email,
-    password
+function Profile(fName, lName, email, password) {
+    this.fName = fName;
+    this.lName = lName;
+    this.email = email
+    this.password = password
 };
-var accounts = [profile];
-fName.addEventListener("input");
-lName.addEventListener("input");
-onyen.addEventListener("input");
-password.addEventListener("input");
-cPassword.addEventListener('input');
+
+var accounts = [Profile];
+fName.addEventListener("input", fName = InputEvent);
+lName.addEventListener("input", lName = InputEvent);
+onyen.addEventListener("input", onyen = InputEvent);
+password.addEventListener("input", password = InputEvent);
+cPassword.addEventListener('input', cPassword = InputEvent);
+
+function storeFirst() {
+    fName = "input";
+}
+
 
 function createAddress(onyen, inputGroupSelect) {
-    var email = onyen + "@" + inputGroupSelect;
+    email = onyen + "@" + inputGroupSelect;
 }
 var emails = [email]
 
-function checkMatch() {
+function checkMatch(password, cPassword) {
     if (password === cPassword && password != null) {
 
     } else {
         alert("Password and Password Confirmation Do Not Match!");
-        break;
     }
 }
 
@@ -48,12 +54,9 @@ function noEmailRepeats() {
 function assureSignUp(fName, lName, onyen, inputGroupSelect, password, cPassword) {
     checkMatch;
     noEmailRepeats;
-    profile.fName;
-    profile.lName;
     emails.push = createAddress(onyen, inputGroupSelect);
-    profile.password;
-
-    accounts.push = profile;
+    var p = new Profile(fName, lName, email, password);
+    accounts.push = p;
 }
 
 signup.addEventListener("clicked", assureSignUp(fName, lName, onyen, inputGroupSelect, password, cPassword));
